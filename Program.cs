3cbfe8d95b9c18fe6fb1b5c33aef22dfa6439753
alexk <alexk@DESKTOP-QL7PNT4>
2024-06-06 15:26:08 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Week_5_Challenge_Lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            int[] nums = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+
+            int n = nums.Length;
+
+            int ans = ( n * (n + 1)) / 2;
+
+
+            int missingValue = ans - nums.Sum();
+
+            Console.WriteLine(missingValue);
+        }
+    }
+}
